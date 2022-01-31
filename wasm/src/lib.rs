6a9efbf25b0c66405cba019650558b64384d4c88
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-01-31 18:46:45 +0100

    towards what could actually work...

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn gen_svg(a: i32, b: i32) -> i32 {
-    return a + b;
+pub fn gen_svg(img: &[u8], din_a4: bool) -> Vec<u8> {
+    let s: String = format!("{din_a4}: {:?}", &img[..10]);
+    s.as_bytes().iter().cloned().collect()
 }
