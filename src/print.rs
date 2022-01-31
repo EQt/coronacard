c9de2f5b73a26eb7e2d4d9ec44e869e85ff6b8b6
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-01-31 04:10:12 +0100

    clippy and fmt

--- src/print.rs
+++ src/print.rs
@@ -1,17 +1,15 @@
 pub(crate) fn print_a4(card: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let inner = xmltree::Element::parse(card.as_bytes())?;
     let print_svg = include_str!("../data/print.svg");
-
+    let inner = xmltree::Element::parse(card.as_bytes())?;
     let mut xml = xmltree::Element::parse(print_svg.as_bytes())?;
-    xml.children.iter_mut().for_each(|tag| match tag {
-        xmltree::XMLNode::Element(img) => {
+    xml.children.iter_mut().for_each(|tag| {
+        if let xmltree::XMLNode::Element(img) = tag {
             if &img.name == "image" {
                 img.attributes.remove("href");
                 img.name = "svg".into();
                 img.children = inner.children.clone();
             }
         }
-        _ => (),
     });
     let mut out = Vec::new();
     xml.write(&mut out)?;
--- src/qrdecode.rs
+++ src/qrdecode.rs
@@ -1,4 +1,6 @@
-pub(crate) fn decode_qr<P: AsRef<std::path::Path>>(path: P) -> Result<String, Box<dyn std::error::Error>> {
+pub(crate) fn decode_qr<P: AsRef<std::path::Path>>(
+    path: P,
+) -> Result<String, Box<dyn std::error::Error>> {
     let img = image::open(path)?.to_luma8();
     // Prepare for detection
     let mut img = rqrr::PreparedImage::prepare(img);
