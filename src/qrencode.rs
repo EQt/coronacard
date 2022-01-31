1698229703117b632463ea81166a9bda54bf31f9
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-01-31 04:30:56 +0100

    fix it...

--- src/qrencode.rs
+++ src/qrencode.rs
@@ -1,17 +1,17 @@
 fn fix_svg_header(qr: String) -> Result<String, Box<dyn std::error::Error>> {
-    {
-        let tag_end = qr.find('>').ok_or("could not find end tag")?;
-        let svg_header = &qr[..tag_end];
-        if svg_header.contains("x=") {
-            None.ok_or("x attribute in svg header")?;
-        }
-        if svg_header.contains("y=") {
-            None.ok_or("y attribute in svg header")?;
-        }
-    }
     let qr = qr
         .strip_prefix(r#"<?xml version="1.0" standalone="yes"?>"#)
         .ok_or("expected prefix not found")?;
+    {
+        let tag_end = qr.find('>').ok_or("could not find end tag")?;
+        let svg_header = &qr[..tag_end];
+        if svg_header.contains(" x=") {
+            None.ok_or(format!("x attribute in svg header: {svg_header}"))?;
+        }
+        if svg_header.contains(" y=") {
+            None.ok_or("y attribute in svg header")?;
+        }
+    }
     Ok(qr
         .replacen(r#"height="255""#, r#"height="51mm" y="1mm""#, 1)
         .replacen(r#"width="255""#, r#"width="51mm" x="1mm""#, 1))
