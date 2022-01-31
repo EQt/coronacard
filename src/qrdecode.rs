fc51f9e6939da5646bf0a979b8ec5a518913a203
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-01-31 04:15:17 +0100

    prefer where syntax

--- src/qrdecode.rs
+++ src/qrdecode.rs
@@ -1,6 +1,7 @@
-pub(crate) fn decode_qr<P: AsRef<std::path::Path>>(
-    path: P,
-) -> Result<String, Box<dyn std::error::Error>> {
+pub(crate) fn decode_qr<P>(path: P) -> Result<String, Box<dyn std::error::Error>>
+where
+    P: AsRef<std::path::Path>,
+{
     let img = image::open(path)?.to_luma8();
     // Prepare for detection
     let mut img = rqrr::PreparedImage::prepare(img);
