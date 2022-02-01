f86478755116cbe4f5a9b5fb0569ec38edc56d94
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-02-01 01:36:49 +0100

    actually implement the algorithm...

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,9 +2,12 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn gen_svg(img: &[u8], din_a4: bool) -> Vec<u8> {
-    format!("{:x}{:x}{:x}\n", img[0], img[1], img[2])
-        .as_bytes()
-        .iter()
-        .cloned()
-        .collect()
+    let code = match coronacard::qr_from_img(img) {
+        Ok(code) => code,
+        Err(_) => return vec![],
+    };
+    match coronacard::svg_from_code(&code, din_a4) {
+        Ok(svg) => svg.into_bytes(),
+        Err(_) => vec![],
+    }
 }
