9cd70ed030ffbafb2a5745493ec55ebd56dff24d
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-02-01 01:26:53 +0100

    most important function into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,25 @@ pub fn default_template() -> String {
     include_str!("../data/template.svg").into()
 }
 
-
-pub fn qr_detect(buf: &[u8]) -> Result<String, Box<dyn std::error::Error>> {
+pub fn qr_from_img(buf: &[u8]) -> Result<String, Box<dyn std::error::Error>> {
     qrdecode::decode_qr_image(&image::load_from_memory(buf)?)
+}
+
+pub fn svg_with_templ(
+    code: &str,
+    din_a4: bool,
+    templ: String,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let vac = Vacc::parse(code)?;
+    let qrsvg = gen_qr_code(code)?;
+    let templ = vac.to_svg(templ, &qrsvg);
+    Ok(if din_a4 {
+        svg::print_a4(&templ)?
+    } else {
+        templ
+    })
+}
+
+pub fn svg_from_code(code: &str, din_a4: bool) -> Result<String, Box<dyn std::error::Error>> {
+    svg_with_templ(code, din_a4, default_template())
 }
