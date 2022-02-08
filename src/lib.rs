c76081b647605b46900a0b74fa4fb9f7a1bcd802
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-02-08 15:14:43 +0100

    finalize ownn error type

--- src/lib.rs
+++ src/lib.rs
@@ -15,22 +15,53 @@ pub fn default_a4_template() -> String {
     include_str!("../data/print.svg").into()
 }
 
-pub fn svg_with_templ(
-    code: &str,
-    pdf: bool,
-    templ: String,
-) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let vac = Vacc::parse(code)?;
-    let qrsvg = gen_qr_code(code)?;
+pub enum Error {
+    Vacc(vacc::VaccErr),
+    GenQr(qrencode::QrEncErr),
+    GenSvg(Box<dyn std::error::Error>),
+    GenPdf(Box<dyn std::error::Error>),
+}
+
+impl std::fmt::Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Vacc(arg0) => f.debug_tuple("Vacc").field(arg0).finish(),
+            Self::GenQr(arg0) => f.debug_tuple("GenQr").field(arg0).finish(),
+            Self::GenSvg(arg0) => f.debug_tuple("GenSvg").field(arg0).finish(),
+            Self::GenPdf(arg0) => f.debug_tuple("GenPdf").field(arg0).finish(),
+        }
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::Vacc(e) => Some(e),
+            Error::GenQr(e) => Some(e),
+            Error::GenSvg(e) => Some(e.as_ref()),
+            Error::GenPdf(e) => Some(e.as_ref()),
+        }
+    }
+}
+
+pub fn svg_with_templ(code: &str, pdf: bool, templ: String) -> Result<Vec<u8>, Error> {
+    let vac = Vacc::parse(code).map_err(|e| Error::Vacc(e))?;
+    let qrsvg = gen_qr_code(code).map_err(|e| Error::GenQr(e))?;
     let templ = vac.to_svg(templ);
-    let templ: String = svg::replace_rect(templ, &qrsvg)?;
+    let templ: String = svg::replace_rect(templ, &qrsvg).map_err(|e| Error::GenSvg(e))?;
     Ok(if pdf {
-        svg::to_pdf(&templ)?
+        svg::to_pdf(&templ).map_err(|e| Error::GenPdf(e))?
     } else {
         templ.into_bytes()
     })
 }
 
-pub fn svg_from_code(code: &str, din_a4: bool) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+pub fn svg_from_code(code: &str, din_a4: bool) -> Result<Vec<u8>, Error> {
     svg_with_templ(code, din_a4, default_template())
 }
