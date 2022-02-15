81ed8f46c560d78eefd74cd60bd2fe1d4251d8a0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-02-15 18:33:50 +0100

    switch to qrenc crate

--- src/qrencode.rs
+++ src/qrencode.rs
@@ -6,10 +6,10 @@
 fn fix_svg_header(svg: &str) -> Result<String, QrEncErr> {
     let mut xml =
         xmltree::Element::parse(svg.as_bytes()).map_err(|e| QrEncErr::QrSvgParse(Box::new(e)))?;
-    xml.attributes.insert("x".into(), "1mm".into());
-    xml.attributes.insert("y".into(), "1mm".into());
-    xml.attributes.insert("width".into(), "51mm".into());
-    xml.attributes.insert("height".into(), "51mm".into());
+    xml.attributes.insert("x".into(), "0mm".into());
+    xml.attributes.insert("y".into(), "0mm".into());
+    xml.attributes.insert("width".into(), "53mm".into());
+    xml.attributes.insert("height".into(), "53mm".into());
     let mut out = Vec::new();
     xml.write_with_config(
         &mut out,
@@ -57,16 +57,8 @@ impl std::error::Error for QrEncErr {
 }
 
 /// Generate a QR code as SVG.
-pub fn gen_qr_code(code: &str) -> Result<String, QrEncErr> {
-    use qrcode::render::svg;
-    use qrcode::QrCode;
-
-    let svg = QrCode::with_error_correction_level(code, qrcode::EcLevel::L)
-        .map_err(|e| QrEncErr::Encode(code.into(), e.into()))?
-        .render()
-        .min_dimensions(200, 200)
-        .dark_color(svg::Color("#000000"))
-        .light_color(svg::Color("#ffffff"))
-        .build();
-    fix_svg_header(&svg)
+pub fn gen_qr_code(code: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let svg = qrenc::svg_from_qr(code)?;
+    let svg = fix_svg_header(&svg)?;
+    Ok(svg)
 }
